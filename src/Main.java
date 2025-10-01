bc2dcf6a25b4f3656a376023fefb008bbca2f3dc
kauansoaresalmeida <kauansoa999@gmail.com>
2025-09-30 22:10:37 -0300

    DIA 30/09

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
+import java.util.Date;
 public class Main {
+    public static void main(String[] args) {
+        UserController uc = new UserController();
+        uc.cadastrarUsuario(new Usuario("Kauan","Soares",new Date(),90028922,'M',"R louca","user","ABCD","email"));
+
+        uc.cadastrarUsuario(new Usuario("Cau","Soares",new Date(),90028922,'M',"R louca","user","ABCD","email"));
+    }
 }
