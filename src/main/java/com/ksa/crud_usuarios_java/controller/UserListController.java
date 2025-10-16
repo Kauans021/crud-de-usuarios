ec3bb4787e7e1689aa3f4b54cffb1a4e498bc00f
kauansoaresalmeida <kauansoa999@gmail.com>
2025-10-16 20:54:09 -0300

    16/10

--- src/main/java/com/ksa/crud_usuarios_java/controller/UserListController.java
+++ src/main/java/com/ksa/crud_usuarios_java/controller/UserListController.java
@@ -1,13 +1,21 @@
 package com.ksa.crud_usuarios_java.controller;
 
+import com.ksa.crud_usuarios_java.MainApplication;
 import com.ksa.crud_usuarios_java.model.Usuario;
+import com.ksa.crud_usuarios_java.service.DatabaseService;
 import com.ksa.crud_usuarios_java.service.UsuarioService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class UserListController {
 
@@ -15,12 +23,12 @@ public class UserListController {
     private TableView<Usuario> tableView;
     @FXML
     private TableColumn<Usuario, String> colNome;
-    @FXML
-    private TableColumn<Usuario, String> colSobrenome;
-    @FXML
-    private TableColumn<Usuario, String> colEmail;
-    @FXML
-    private TableColumn<Usuario, String> colTelefone;
+    //    @FXML
+//    private TableColumn<Usuario, String> colSobrenome;
+//    @FXML
+//    private TableColumn<Usuario, String> colEmail;
+//    @FXML
+//    private TableColumn<Usuario, String> colTelefone;
     @FXML
     private TableColumn<Usuario, String> colLogin;
     @FXML
@@ -41,58 +49,82 @@ public class UserListController {
     }
 
     public void atualizarStatusConexao(){
-
+        boolean isConnected = DatabaseService.testarConexao();
+        if (isConnected){
+            statusLabel.setText("DB Status: connected");
+            statusLabel.setStyle("-fx-text-fill: green");
+            syncButton.setText("refresh");
+            syncButton.setDisable(true);
+            syncButton.setVisible(false);
+        } else{
+            statusLabel.setText("DB Status: offline");
+            statusLabel.setStyle("-fx-text-fill: red");
+            syncButton.setText("retry");
+            syncButton.setDisable(false);
+            syncButton.setVisible(true);
+        }
     }
     @FXML
     public void handleSincronizar(){
+        usuarioService.sincronizarComBanco();
+        carregarDadosTabela();
 
     }
 
     public void carregarDadosTabela(){
-        if (usuarioService.isDbloaded()) {
-            colNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
-            colSobrenome.setCellValueFactory(new PropertyValueFactory<>("sobrenome"));
-            colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
-            colTelefone.setCellValueFactory(new PropertyValueFactory<>("telefone"));
-            colLogin.setCellValueFactory(new PropertyValueFactory<>("login"));
+        colNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+//            colSobrenome.setCellValueFactory(new PropertyValueFactory<>("sobrenome"));
+//            colEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+//            colTelefone.setCellValueFactory(new PropertyValueFactory<>("telefone"));
+        colLogin.setCellValueFactory(new PropertyValueFactory<>("login"));
 
-            obsUsuario = FXCollections.observableArrayList(usuarioService.listarUsuarios());
-            tableView.setItems(obsUsuario);
-        } else{
-            System.out.println("Dados não foram carregados.");
-        }
+        obsUsuario = FXCollections.observableArrayList(usuarioService.listarUsuarios());
+        tableView.setItems(obsUsuario);
+        adicionarBotoesDeAcao();
+        atualizarStatusConexao();
     }
 
     public void adicionarBotoesDeAcao(){
         colAcoes.setCellFactory(pram -> new TableCell<>() {
             private final Button btnEditar = new Button("Editar");
             private final Button btnExcluir = new Button("Excluir");
-            private final HBox pane = new HBox(5,btnEditar,btnExcluir);
+            private final HBox panel = new HBox(5,btnEditar,btnExcluir);
             {
                 btnEditar.setOnAction(event ->{
                     Usuario usuario = getTableView().getItems().get(getIndex());
                     abrirFormularioUsuario(usuario);
                 });
-
                 btnExcluir.setOnAction(event ->{
                     Usuario usuario = getTableView().getItems().get(getIndex());
                     usuarioService.excluirUsuario(usuario);
                     carregarDadosTabela();
                 });
             }
-
-
+            @Override
+            protected void updateItem(Void item, boolean empty){
+                super.updateItem(item,empty);
+                setGraphic(empty ? null : panel);
+            }
         });
-
     }
-
     @FXML
-    public void handleAdicionarUsuario(){
-
+    public void handleAdicionarUsuario() {
+        abrirFormularioUsuario(null);
     }
-
     public void abrirFormularioUsuario(Usuario usuario){
-
+        try{
+            FXMLLoader loader = new FXMLLoader(MainApplication.class.getResource("user-form-view.fxml"));
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(new Scene(loader.load()));
+            UserFormController controller = loader.getController();
+            controller.setUsuario(usuario);
+            controller.setStage(stage);
+            stage.showAndWait();
+            carregarDadosTabela();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
 }
